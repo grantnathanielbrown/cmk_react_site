44377ce5c25f69e30ab679de3eb09d10d0511da7
Grant Brown <grant@Grants-MacBook-Pro.local>
2018-12-16 18:58:05 -0500

    changed width on middle section in-home, added sample image for mom, subtitle

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 // import Footer from '../Global/Footer';
 import Header from '../Global/Header';
-
+import RyanGosling from './RyanGosling10-19.JPG';
 export default class Home extends Component {
   render() {
     return (      
-<div class="row">
-    <Header></Header>      
-    <div class="col-lg-1">
-    </div>
-    <div class="col-lg-2">
-      <img src="C:\Users\Grant\Desktop\hohohohoho\220px-Ryan_Gosling_by_Gage_Skidmore.jpg"/>
-    Christen M Kerr
-    </div>
-    <div class="col-lg-8">
-    <p class="welcome">WELCOME TO MY PRACTICE</p>
-    <p class="description">Dr. Kerr is a compassionate physician who calls upon many years of experience plus the latest in cutting edge science to design unique treatments for each patient.  A thorough understanding of each individual allows for treatment that may include psychotherapy and medication management, as needed.  Therapy is delivered in an environment of confidentiality and support.</p>
-    </div>
-    <div class="col-lg-1">
-    </div>
+<div>
+    <Header></Header>
+    <div className="sideways">
+<figure>
+    <img src={RyanGosling} alt='missing' />
+    <figcaption>Christen M. Kerr</figcaption>
+</figure>
+      
 
+    {/* <p className="welcome">WELCOME TO MY PRACTICE</p> */}
+    <p className="description">Dr. Kerr is a compassionate physician who calls upon many years of experience plus the latest in cutting edge science to design unique treatments for each patient.  
+    A thorough understanding of each individual allows for treatment that may include psychotherapy and medication management, as needed. 
+    Therapy is delivered in an environment of confidentiality and support.
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+    </div>
 </div>    
 
     )
