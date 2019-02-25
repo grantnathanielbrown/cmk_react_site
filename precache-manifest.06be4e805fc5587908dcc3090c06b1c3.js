b5b5d398e6f5fb9c1f3643f0bc0adf6effd8d58f
Grant Brown <grant@Grants-MacBook-Pro.local>
2019-02-24 20:36:16 -0500

    Updates

---
+++ precache-manifest.06be4e805fc5587908dcc3090c06b1c3.js
@@ -0,0 +1,101 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cafc74addc983d51685407c8dcce212",
+    "url": "/static/media/8151987436_d05fa6aa07_b.0cafc74a.jpg"
+  },
+  {
+    "revision": "b89fb9a1f8dc0bf3e197",
+    "url": "/static/css/main.d2a816a2.chunk.css"
+  },
+  {
+    "revision": "5775f712ea059548f4f7c9a8b010e22d",
+    "url": "/static/media/35459605-medical-symbol-caduceus-with-snakes-and-wings.5775f712.jpg"
+  },
+  {
+    "revision": "e8cfd68811a3c73cc049",
+    "url": "/static/js/1.e8cfd688.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6468f5ab7bcc8d8ec8614f63fa5567ec",
+    "url": "/static/media/ChristenKerr-245.6468f5ab.JPG"
+  },
+  {
+    "revision": "284abd18a82161fea2e4814e2c609756",
+    "url": "/static/media/hey.284abd18.jpg"
+  },
+  {
+    "revision": "f5494ed29f63fd5fa0288a13e8b55f77",
+    "url": "/static/media/serenity.f5494ed2.jpeg"
+  },
+  {
+    "revision": "7cc97c8f93d795d19cfa0f49e0a0b60d",
+    "url": "/static/media/bike.7cc97c8f.png"
+  },
+  {
+    "revision": "e5341e039f8a79e378d6aa3a989a1833",
+    "url": "/static/media/hike.e5341e03.png"
+  },
+  {
+    "revision": "53ce7be8e306c0d65a428defecfc7a38",
+    "url": "/static/media/island.53ce7be8.png"
+  },
+  {
+    "revision": "e249f1497f42eddf2fcd3dc6ffdfc6be",
+    "url": "/static/media/istockphoto-841246134-612x612.e249f149.jpg"
+  },
+  {
+    "revision": "b89fb9a1f8dc0bf3e197",
+    "url": "/static/js/main.b89fb9a1.chunk.js"
+  },
+  {
+    "revision": "534640fde0cbb6541359ebfdaffb6714",
+    "url": "/static/media/Washington-DC-skyline.534640fd.jpg"
+  },
+  {
+    "revision": "f331ce1c350ddc2ad37e3bf0a267d6a9",
+    "url": "/static/media/cc32e202-8685-48b5-bf52-c45da58bc4f6-large16x9_MO1_1838Edit.f331ce1c.jpg"
+  },
+  {
+    "revision": "22821845a76b15e3aece7e666bdbae03",
+    "url": "/static/media/genetic-testing-banner.22821845.jpg"
+  },
+  {
+    "revision": "d283cd466095dda2de4a610f207bd06d",
+    "url": "/static/media/Sleeping.d283cd46.jpeg"
+  },
+  {
+    "url": "/static/media/1fd32a5a7af89ce657a1129e583d47be.1fd32a5a.jpg"
+  },
+  {
+    "revision": "6d92c7841c327dc68763f98f5fb49ece",
+    "url": "/static/media/psychotherapy-banner.6d92c784.jpg"
+  },
+  {
+    "revision": "34509bfee74e95e64048370b54465854",
+    "url": "/static/media/waves_cannon_07.34509bfe.jpg"
+  },
+  {
+    "revision": "de14134097f090f3b2f59d53105155b2",
+    "url": "/static/media/Senior-Travel-Problems.de141340.jpg"
+  },
+  {
+    "revision": "a9f306440c84d0d0a74e2e44f748757d",
+    "url": "/static/media/Hugo bend (front view).a9f30644.JPG"
+  },
+  {
+    "revision": "b485fee8b51edf581243e905dee4c181",
+    "url": "/static/media/depression-be.b485fee8.jpg"
+  },
+  {
+    "revision": "e8cfd68811a3c73cc049",
+    "url": "/static/css/1.1b2cc9b3.chunk.css"
+  },
+  {
+    "revision": "e75c1bb4a1835b5fd03f54250f9bd326",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/cmk_react_site/precache-manifest.891b78e38b6616b876ee06bf96111a93.js"
+  "/precache-manifest.06be4e805fc5587908dcc3090c06b1c3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cmk_react_site/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
