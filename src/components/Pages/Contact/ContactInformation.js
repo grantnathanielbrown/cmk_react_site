dc9d1bf7a6d6cd4854635e97d4c57b140c777282
Grant Brown <grant@Grants-MacBook-Pro.local>
2019-03-19 02:31:27 -0400

    changed contact page to sound a bit smarter

--- src/components/Pages/Contact/ContactInformation.js
+++ src/components/Pages/Contact/ContactInformation.js
@@ -13,11 +13,9 @@ export default class ContactInformation extends Component {
       <meta name="description" content="This page describes the directions to Christen's office as well as her contact information." />
       <link rel="canonical" href="http://christenkerrmd.com/#/Contact-information" />
       </Helmet>
-        <h2>1307 Dolley Madison Blvd</h2>
-        <h2>Suite 3C</h2>
-        <h2>McLean, VA 22101</h2>
-        <h2>New patients please call for an appointment at: <a href="tel:571-835-6474">571-835-6474</a></h2>
-        <h2>Office: <a href="703-734-7961">703-734-7961</a></h2>
+        <h2>Christen's office is located at 1307 Dolley Madison Blvd Suite 3C McLean, VA 22101. <a href="https://www.google.com/maps/place/1307+Dolley+Madison+Blvd,+McLean,+VA+22101/@38.9402127,-77.1787593,17z/data=!3m1!4b1!4m5!3m4!1s0x89b7b55f28762b15:0xf44a32d9017eb129!8m2!3d38.9402127!4d-77.1765706"><i class="fas fa-map-marker-alt"></i></a></h2>
+        <h2>If you are a new patient and would like to schedule an initial consultation, please call <a href="tel:571-835-6474">571-835-6474</a>.</h2>
+        <h2>To contact the office otherwise, please call <a href="703-734-7961">703-734-7961</a>.</h2>
         <h2>Fax: <a href="tel:703-734-7962">703-734-7962</a></h2>
         <MapContainer></MapContainer>
       </div>
